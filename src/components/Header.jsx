0e020e464a6ecc05b0a291d252ffe5d41f0ba618
shimul705 <freelancershimul172@gmail.com>
2024-03-24 03:30:21 +0600

    added responsive animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
 import { GoDotFill } from "react-icons/go";
+import { TbLineDotted } from "react-icons/tb";
+import { FaReact } from "react-icons/fa6";
 
 const Header = () => {
   return (
-    <div className=' bg-[#F5F5F5] relative'>
+    <div className=' bg-[#F5F5F5]'>
+
+      <div className='relative'>
+        <div className='hidden md:block'>
+          <div className='text-4xl top-36 left-48  opacity-90 text-[#262262] absolute bottom-0'>
+            <GoDotFill className='prop1' />
+          </div>
+          <div className='absolute top-44 left-40 h-2 w-16'>
+            <hr className='border-t-4 border-blue-950' />
+          </div>
+          <div className='absolute top-48 left-40 h-2 w-16'>
+            <hr className='border-t-4 border-blue-950' />
+          </div>
+        </div>
+
+        {/* React Spin */}
+        <div className='absolute md:top-[12.8rem] md:right-[30.2rem] top-[28.8rem] right-[6.2rem] text-6xl text-blue-800'>
+          <FaReact className='reactspin' />
+        </div>
+      </div>
+
+
+
       <div className='md:w-8/12 md:text-start text-center px-4 md:px-0 py-20 m-auto flex flex-col md:flex-row items-center'>
         {/* Left Side */}
         <div className='md:w-1/2'>
@@ -18,17 +42,29 @@ const Header = () => {
         </div>
       </div>
 
-      <div className='text-4xl  opacity-90 text-[#262262] absolute top-36 left-48'>
-        <GoDotFill className='prop1' />
+      <div className='relative'>
+
+        {/* Right dottet animation */}
+        <div className='absolute body opacity-70 bottom-0 text-6xl right-20'>
+          <div className='relative'>
+            <TbLineDotted className='absolute bottom-0' />
+            <TbLineDotted className='absolute bottom-2' />
+            <TbLineDotted className='absolute bottom-4' />
+            <TbLineDotted className='absolute bottom-6' />
+          </div>
+          <div className='relative right-10'>
+            <TbLineDotted className='absolute bottom-0' />
+            <TbLineDotted className='absolute bottom-2' />
+            <TbLineDotted className='absolute bottom-4' />
+            <TbLineDotted className='absolute bottom-6' />
+          </div>
+
+        </div>
 
       </div>
-      <div className='absolute top-44 left-40 h-2 w-16'>
-        <hr className='border-t-4 border-blue-950' />
-      </div>
-      <div className='absolute top-48 left-40 h-2 w-16'>
-        <hr className='border-t-4 border-blue-950' />
-      </div>
-      
+
+
+
 
 
 
--- src/components/Hex.jsx
+++ src/components/Hex.jsx
@@ -4,7 +4,7 @@ const Hex = () => {
   return (
     <div className='m-auto pt-24 pb-52 w-10/12 md:w-7/12 grid grid-cols-2 md:grid-cols-6 gap-10'>
       
-      <div className='body'>
+      <div className='body ani1'>
         <div className='hexagon'></div>
         <div className='z-0 absolute flex flex-col justify-center items-center font-medium '>
           <img className='max-h-16 max-w-20' src="/React.png" alt="" />
@@ -12,7 +12,7 @@ const Hex = () => {
         </div>
       </div>
 
-      <div className='body'>
+      <div className='body ani1'>
         <div className='hexagon'></div>
         <div className='z-0 absolute flex flex-col justify-center items-center font-medium  '>
           <img className='max-h-16 max-w-20' src="/vue.png" alt="" />
@@ -20,70 +20,70 @@ const Hex = () => {
         </div>
       </div>
 
-      <div className='body'>
+      <div className='body ani1'>
         <div className='hexagon'></div>
         <div className='z-0 absolute flex flex-col justify-center items-center font-medium'>
           <img className='h-16' src="/figma.png" alt="" />
           <h1 className='text-lg mt-2 '>Figma</h1>
         </div>
       </div>
-      <div className='body'>
+      <div className='body ani1'>
         <div className='hexagon'></div>
         <div className='z-0 absolute flex flex-col justify-center items-center font-medium '>
           <img className='max-h-16 max-w-24' src="/Wordpress.png" alt="" />
           <h1 className='text-lg mt-2 '>Wordpress</h1>
         </div>
       </div>
-      <div className='body'>
+      <div className='body ani1'>
         <div className='hexagon'></div>
         <div className='z-0 absolute flex flex-col justify-center items-center font-medium '>
           <img className='max-h-16 max-w-24' src="/laravel.png" alt="" />
           <h1 className='text-lg mt-2 '>Laravel</h1>
         </div>
       </div>
-      <div className='body'>
+      <div className='body ani1'>
         <div className='hexagon'></div>
         <div className='z-0 absolute flex flex-col justify-center items-center font-medium '>
           <img className='max-h-16 max-w-24' src="/mongo.png" alt="" />
           <h1 className='text-lg mt-2 '>MongoDB</h1>
         </div>
       </div>
-      <div className='body'>
+      <div className='body ani1'>
         <div className='hexagon'></div>
         <div className='z-0 absolute flex flex-col justify-center items-center font-medium '>
           <img className='max-h-16 max-w-24' src="/mysql.png" alt="" />
           <h1 className='text-lg mt-2 '>MySQL</h1>
         </div>
       </div>
-      <div className='body'>
+      <div className='body ani1'>
         <div className='hexagon'></div>
         <div className='z-0 absolute flex flex-col justify-center items-center font-medium '>
           <img className='max-h-16 max-w-24' src="/next.png" alt="" />
           <h1 className='text-lg mt-2 '>NEXT.JS</h1>
         </div>
       </div>
-      <div className='body'>
+      <div className='body ani1'>
         <div className='hexagon'></div>
         <div className='z-0 absolute flex flex-col justify-center items-center font-medium'>
           <img className='max-h-16 max-w-24' src="/node.png" alt="" />
           <h1 className='text-lg mt-2 '>node.js</h1>
         </div>
       </div>
-      <div className='body'>
+      <div className='body ani1'>
         <div className='hexagon'></div>
         <div className='z-0 absolute flex flex-col justify-center items-center font-medium'>
           <img className='max-h-16 max-w-24' src="/php.png" alt="" />
           <h1 className='text-lg mt-2 '>php</h1>
         </div>
       </div>
-      <div className='body'>
+      <div className='body ani1'>
         <div className='hexagon'></div>
         <div className='z-0 absolute flex flex-col justify-center items-center font-medium'>
           <img className='max-h-16 max-w-24' src="/angular.png" alt="" />
           <h1 className='text-lg mt-2 '>Angular.js</h1>
         </div>
       </div>
-      <div className='body'>
+      <div className='body ani1'>
         <div className='hexagon'></div>
         <div className='z-0 absolute flex flex-col justify-center items-center font-medium'>
           <img className='max-h-16 max-w-24' src="/tailwind.png" alt="" />
